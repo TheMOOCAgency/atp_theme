f22c065cc50a181756d17b5f697d8ecf10027e0d
root <root@ip-172-31-20-185.eu-west-1.compute.internal>
2018-10-09 14:26:45 +0000

    upload_csv_save

--- cms/static/js/atp/invite.js
+++ cms/static/js/atp/invite.js
@@ -1,145 +1,218 @@
-define(['domReady', 'jquery', 'underscore','jquery.ui'],function(domReady, $, _) {
-  var onReady = function() {
-    // method preview csv file
-    var ok_csv = true;
 
-    function csv_preview(input,output,next) {
-      var reader = new FileReader();
-      var fileinput = document.getElementById(input);
-      reader.onload = function(){
-        var lines = this.result.split('\n');
-        var out = $('#'+output);
-        var limit = 0;
-        out.html();
-        for(var line = 0; line < lines.length; line++){
-          if(limit < 6) {
-            columns = lines[line].split(',');
-            var row_id = 'csv_row_'+line;
-            var paragraphe = "<p id='"+row_id+"'></p>";
-            out.append(paragraphe);
-            var cur_row = $('#'+row_id);
-            var email_column;
-            if(line > 0) {
-              email_column = "<span class='email_row'>"+columns[0]+"</span>";
-              cur_row.append(email_column);
-            }else{
-              email_column = "<span>"+columns[0]+"</span>";
-              cur_row.append(email_column);
-            }
-            for(var i = 1;i<columns.length;i++) {
-              var span = "<span>"+columns[i]+"</span>";
-              cur_row.append(span);
-            }
-            //out.append('<p><span>'+lines[line].split(',').join('</span><span>')+'</span><p>');
-          }else{
-            line = lines.length - 1;
+//////////////////////////////////////////////////// CHECK CSV FILE //////////////////////////////////////////////////
+function ParseCsvFile(evt) {
+  $("#csv_import_error").css('display','none');
+  $('#csv_import_feedback').html('');
+  $('#csv_import').html('');
+  $('#csv_import_preview').css('display','none');
+  $('#csv_import_feedback').css('display','none');
+  final_valid_records={};
+  final_error_records={};
+  $('#register_user_btn').css('display','none');
+  var file = evt.target.files[0];
+  var fileread = new FileReader();
+  fileread.onload = function(e) {
+    encoding_type=jschardet.detect(e.target.result).encoding;
+    Papa.parse(file, {
+      header: true,
+      dynamicTyping: true,
+      encoding: encoding_type,
+      complete: function(results) {
+        data = results;
+        csv_users=results['data'];
+        csv_users_cleaned = clean_maj(csv_users);
+        valid_header=check_valid_header(csv_users_cleaned);
+        if(valid_header==true){
+          check_results = check_csv_errors(csv_users_cleaned);
+          final_valid_records = check_results['valid_records'];
+          final_error_records = check_results['errors'];
+          if(final_error_records.length>0){
+            manage_error_display(final_error_records);
           }
-        };
-        out.find('p').each(function(){
-          var This = $(this);
-          var text = This.text();
-          if(text == '') {
-            This.remove();
+          else{
+            display_csv_rows(final_valid_records);
+            $('#register_user_btn').css('display','block');
           }
-        })
-        $('.email_row').each(function(){
-          var This = $(this);
-          var text = This.text();
-          var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-          if(!re.test(text)) {
-            ok_csv = false;
-            This.addClass("is_error");
-          }
-        })
-        out.find('span').attr('style','');
-        $('#output_section').show();
-        if(ok_csv) {
-            $('#'+next).show();
-        }else{
-          $('#pop_up_mail_fail').addClass('is_show');
+        }
+        else{
+          manage_error_display(valid_header);
         }
       }
-      reader.readAsText(fileinput.files[0]);
-      return "canard";
-    };
-    function invite() {
-      this.constructor = function(body,action,data,checkbox) {
-        this.body = body;
-        this.action = action;
-        this.data = data;
-        this.checkbox = checkbox;
-      },
-      // set action
-      this.setAction = function(val) {
-        this.action = val;
-      },
-      this.getData = function() {
-        return this.data;
-      },
-      // upload and pre-register only users
-      this.register_only = function(id,file) {
-        var This = $('#'+id);
-        This.click(function(){
-          var data = new FormData($(file).get(0));
-          data.append('request_type','register_only');
-          this.data = data;
-        })
-      },
-      // load csv file to custom user input
-      this.load_csv = function(file,action) {
-        var data = new FormData($(file).get(0));
-        data.append('request_type',action);
-        data
-        this.data = data;
-      },
-      // action on use file upload
-      this.file_up_input = function(id,next) {
-        var This = $('#'+id);
-        This.change(function(){
-          $('#pop_up_mail_fail').removeClass('is_show');
-          $('#pop_up_invite').removeClass('is_show');
-          var val = This.attr('value');
-          var split = val.split('.');
-          var long = split.length - 1;
-          var check = split[long];
-          if(check.indexOf('csv') == -1) {
-            $('#pop_up_mail_fail').addClass('is_show');
-          }else{
-          csv_preview('invite_participant','output',next)
-          }
-        })
+    });
+  };
+  fileread.readAsText(file);
+}
+
+function clean_maj(csv_users){
+  i=0;
+  csv_users_cleaned={};
+  for (user in csv_users){
+    user_obj =csv_users[user];
+    var key, keys = Object.keys(user_obj);
+    var n = keys.length;
+    var new_user_obj={}
+    while (n--) {
+      key = keys[n];
+      new_user_obj[key.toLowerCase()] = user_obj[key];
+    }
+    csv_users_cleaned[i]=new_user_obj;
+    i+=1;
+  }
+  return csv_users_cleaned;
+}
+
+function check_valid_header(csv_users_cleaned){
+  valid_header=true;
+  errors={};
+  errors['missing_header']=[];
+  for(i=0;i<required_fields.length;i++){
+    if (!(required_fields[i] in csv_users_cleaned[0])){
+      errors['missing_header'].push(required_fields[i]);
+    }
+  }
+  if(errors['missing_header'].length>0){
+    valid_header=errors;
+  }
+  return valid_header;
+}
+
+function check_csv_errors(csv_users_cleaned){
+  errors=[];
+  valid_records=[];
+  check_results={};
+  for (user in csv_users_cleaned){
+    user_data=csv_users_cleaned[user];
+    valid=true
+    user_data['missing_fields']=[];
+    user_data['line']=parseInt(user)+2;
+    console.log(user_data);
+    for (var i = 0; i < required_fields.length; i++){
+      if(user_data[required_fields[i]]==undefined && user_data[required_fields[i]]==null){
+        valid=false;
+        user_data['missing_fields'].push(required_fields[i]);
       }
-    };
+      else if(required_fields[i]=='email' && !validateEmail(user_data['email'])){
+        valid=false;
+        user_data['invalid_email']=true;
+      }
+    }
+    if(valid==true){
+      valid_records.push(user_data);
+    }
+    else if(!isempty(user_data)){
+      errors.push(user_data);
+    }
+  }
+  check_results['valid_records']=valid_records;
+  check_results['errors']=errors;
+  return check_results;
+}
 
+function display_csv_rows(csv_users_cleaned){
+  $('#csv_import').css('display','block');
+  set_header();
+  if(times_displayed<1){
+    dynatable = $('#csv_import').dynatable({
+    dataset: {
+        records: csv_users_cleaned
+    }
+    }).data('dynatable');
+  }
+  else{
+    dynatable.settings.dataset.originalRecords = csv_users_cleaned;
+    dynatable.process();
+  }
+  times_displayed+=1;
+  $('.dynatable-active-page a').addClass('primary-color-bg');
+  $('#csv_import_preview').css('display','block');
+}
 
-    /* action on submit form */
-    var submit = '#upload_form_participant';
-    var mail_invite = new invite();
-    mail_invite.file_up_input('invite_participant','choise_from_file');
-    // register users from csv
-    $('#register_from_csv').click(function(){
-      $('#pop_up_invite').removeClass('is_show');
-      mail_invite.load_csv(submit,'register_only');
-      var path = window.location.path;
-      var data = mail_invite.getData();
-      $.ajax({
-        url: path,
-        data: data,
-        cache: false,
-        contentType: false,
-        processData: false,
-        type: 'POST',
-        success: function(data){
-          var retour = data.message;
-          $('#pop_up_invite').addClass('is_show');
+function manage_error_display(errors){
+  $('#csv_import_error').css('display','block');
+  $('#csv_import').css('display','none');
+  text_error='';
+
+  if('missing_header' in errors){
+    text_errors+='<p>'+wording_incorrect_header+' :</p><ul>';
+    for(missing_header in errors['missing_header']){
+      text_error+='<li>'+errors['missing_header'][missing_header]+'</li>';
+    }
+    text_error+='</ul>';
+  }
+  else{
+    text_error+='<p>'+wording_incorrect_info+' :</p><ul>';
+    for(user in errors){
+      text_error+='<li>'+wording_ligne+' '+ errors[user]['line']+' '+(errors[user]['email']||'')+' : ';
+      if('invalid_email' in errors[user]){
+        text_error+="L'email n'est pas valide. "
+      }
+      else if ('missing_fields' in errors[user]){
+        for(i=0;i<errors[user]['missing_fields'].length;i++){
+          text_error+=errors[user]['missing_fields'][i]+' '+wording_missing+'. ';
         }
-      })
+      }
+      text_error+='</li>';
+    }
+    text_error+='</ul>';
+  }
+  $("#csv_import_error").html(text_error);
+}
+
+
+
+
+function set_header(){
+  header_fields = ['email','first_name','last_name','level_1','level_2','level_3','level_4'];
+  $('#csv_import').append('<thead><tr></tr></thead>');
+  for (i=0;i<header_fields.length;i++){
+      $('#csv_import thead tr').append('<th class="primary-color-bg white-border white-text">'+header_fields[i]+'</th>');
+  }
+}
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function isempty(obj) {
+    for (var key in obj) {
+        if (obj[key] !== null && obj[key] != "" && obj[key] != undefined){
+            return false;
+    }
+    return true;
+  }
+}
+
+//////////////////////////////////////////// ACTIONS //////////////////////////////////////
+
+//Check csv on submit
+$(document).ready(function () {
+  times_displayed=0;
+  required_fields = ['email','first_name','last_name'];
+  $("#invite_participant").change(ParseCsvFile);
+
+  // register users from csv
+  $('#register_from_csv').on('click',function(){
+    var csrftoken = $("#csrf").html();
+    var path = window.location.path;
+    var data = new FormData();
+    data.append('file',$('#invite_participant')[0].files[0])
+    data.append('request_type','register_only');
+    $.ajax({
+      url:path,
+      type: 'POST',
+      processData: false,
+      contentType: false,
+      headers:{
+        "X-CSRFToken": csrftoken
+      },
+      data:data,
+      success:function(data){
+        $('#csv_import_feedback').html('<p class="text-center">'+wording_prisencompte+'</p>');
+        $('#csv_import_preview').css('display','none');
+        $('#csv_import_feedback').css('display','block');
+        $('#register_user_btn').css('display','none');
+      }
     })
-    /* svg color */
-    $('.svg_bis').contents().find('svg').attr("fill",'#06144D');
-  };
-  domReady(onReady);
-  return {
-      onReady: onReady
-  };
-})
+  })
+});
